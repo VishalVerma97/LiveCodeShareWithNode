596bb4dcf8d2d491b16630db2beca511db8144c5
Vishal Verma <vishalverma@SLVISHALV.local>
2021-04-21 00:23:56 +0530

    adding styles files

--- middlewares/appMiddleware.js
+++ middlewares/appMiddleware.js
@@ -1,6 +1,9 @@
 const bodyParser = require('body-parser');
+const express = require('express');
+const process = require('process');
 
 module.exports = function(app) {
+  app.use(express.static(process.cwd()));
   app.use(bodyParser.urlencoded({extended: true}));
   app.use(bodyParser.json());
 }
